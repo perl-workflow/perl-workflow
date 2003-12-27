e384db639806300e893cbcb91f86aa5fccacfcdb
Chris Winters <chris.winters@gmail.com>
2003-12-27 19:05:14 +0000

    add a history entry, add a workflow_ticket record

--- workflow/eg/ticket/App/Action/TicketCreate.pm
+++ workflow/eg/ticket/App/Action/TicketCreate.pm
@@ -4,7 +4,9 @@ package App::Action::TicketCreate;
 
 use strict;
 use base qw( Workflow::Action );
-use Log::Log4perl qw( get_logger );
+use Log::Log4perl       qw( get_logger );
+use Workflow::Exception qw( persist_error );
+use Workflow::Factory   qw( FACTORY );
 
 $App::Action::TicketCreate::VERSION  = sprintf("%d.%02d", q$Revision$ =~ /(\d+)\.(\d+)/);
 
@@ -21,22 +23,41 @@ sub execute {
     $log->debug( "Action '", $self->name, "' with class '", ref( $self ), "' executing..." );
     my $creator = $self->param( 'creator' ) || $context->param( 'current_user' );
     my $ticket = App::Ticket->new({
+        status      => $wf->state,
         subject     => $self->param( 'subject' ),
         description => $self->param( 'description' ),
         creator     => $creator,
         due_date    => $self->param( 'due_date' ),
         last_update => $self->param( 'last_update' ),
     });
-    $ticket->status( $wf->state );
-    $ticket->save;
+    $ticket->create;
     $context->param( ticket => $ticket );
     $log->info( "Ticket created correctly with ID ", $ticket->id );
-#    my $sql = qq{
-#      INSERT INTO workflow_ticket ( workflow_id, ticket_id )
-#      VALUES ( $ticket->{workflow_id}, $ticket->{ticket_id} )
-#    };
-#    $ticket->global_datasource_handle->do( $sql );
-#    $log->info( "Link table record inserted correctly" );
+    my $sql = q{
+      INSERT INTO workflow_ticket ( workflow_id, ticket_id )
+      VALUES ( ?, ? )
+    };
+    $log->debug( "Will run SQL\n$sql" );
+    $log->debug( "Will use parameters: ", join( ', ', $wf->id, $ticket->id ) );
+
+    my $dbh = FACTORY->get_persister( 'TestPersister' )->handle;
+    my ( $sth );
+    eval {
+        $sth = $dbh->prepare( $sql );
+        $sth->execute( $wf->id, $ticket->id );
+    };
+    if ( $@ ) {
+        persist_error "Failed to save additional ticket info: $@";
+    }
+    $log->info( "Link table record inserted correctly" );
+    $wf->add_history(
+        Workflow::History->new({
+            action      => 'Create ticket',
+            description => 'New ticket created',
+            user        => $creator,
+            state       => $wf->state,
+        })
+    );
 }
 
 1;
