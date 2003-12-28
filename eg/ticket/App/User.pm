f75098be62601a449e3071d0a857a80c378a3a76
Chris Winters <chris.winters@gmail.com>
2003-12-28 21:51:42 +0000

    add simple source class to return users

---
+++ eg/ticket/App/User.pm
@@ -0,0 +1,22 @@
+package App::User;
+
+# $Id$
+
+use strict;
+
+$App::User::VERSION  = sprintf("%d.%02d", q$Revision$ =~ /(\d+)\.(\d+)/);
+
+my %USERS = (
+    Stu => 'Stu Nahan',
+    Mel => 'Mel Ott',
+    Irv => 'Irv Cross',
+    Bob => 'Bobby Orr',
+    Joe => 'Joe Morgan',
+    Ric => 'Ric Ocasek'
+);
+
+sub get_possible_values {
+    return map { { value => $_, label => $USERS{ $_ } } } sort keys %USERS;
+}
+
+1;
