31904c549b8adf031ea04c0e05caa544f86f74e4
Chris Winters <chris.winters@gmail.com>
2004-01-02 13:01:06 +0000

    add CSV structure

---
+++ workflow/struct/workflow_csv.sql
@@ -0,0 +1,17 @@
+CREATE TABLE workflow (
+  workflow_id       varchar(8) not null primary key,
+  type              varchar(50) not null,
+  state             varchar(30) not null,
+  last_update       varchar(20)
+);
+
+CREATE TABLE workflow_history (
+  workflow_hist_id  varchar(8) not null primary key,
+  workflow_id       varchar(8) not null,
+  action            varchar(25) not null,
+  description       varchar(255),
+  state             varchar(30) not null,
+  user              varchar(50),
+  history_date      varchar(20)
+);
+
