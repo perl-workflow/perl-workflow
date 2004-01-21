84d9ea4f24d69da72ebf384947f76dae42ce338b
Chris Winters <chris.winters@gmail.com>
2004-01-21 13:04:29 +0000

    add shortcuts for checking info in statement tracker and for workflow history entries in particular

--- t/TestUtil.pm
+++ t/TestUtil.pm
@@ -5,6 +5,7 @@ package TestUtil;
 use strict;
 use DateTime;
 use Log::Log4perl qw( get_logger );
+use Test::More;
 
 my $LOG_FILE  = 'workflow_tests.log';
 my $CONF_FILE = 'log4perl.conf';
@@ -35,6 +36,39 @@ sub set_new_ticket_context {
     }
 }
 
+sub check_workflow_history {
+    my ( $class, $tracker, $values ) = @_;
+    $class->check_tracker(
+        $tracker, 'create workflow history',
+        qr/^INSERT INTO workflow_history \( workflow_id, action, description, state, user, history_date, workflow_hist_id \)/,
+        [ 'workflow ID', 'action', 'description',
+          'state', 'user', 'current date',
+          'random ID of correct length' ],
+        $values );
+}
+
+sub check_tracker {
+    my ( $class, $tracker, $tracker_desc, $sql_pattern, $names, $values ) = @_;
+    like( $tracker->statement, $sql_pattern,
+          "Statement matches ($tracker_desc)" );
+    my $track_params = $tracker->bound_params;
+    my $num_params = scalar @{ $names };
+    is( scalar @{ $track_params }, $num_params,
+        "Correct number ($num_params) of parameters bound ($tracker_desc)" );
+    for ( my $i = 0; $i < $num_params; $i++ ) {
+        my $this_name = ( $i == 0 )
+                        ? "Bound parameter for '$names->[ $i ]' matches"
+                        : "...for '$names->[ $i ]' matches";
+        my @to_compare = ( ref( $values->[ $i ] ) eq 'CODE' )
+                           ? $values->[ $i ]->( $track_params->[ $i ] )
+                           : ( $track_params->[ $i ], $values->[ $i ] );
+        is( $to_compare[0], $to_compare[1], $this_name );
+    }
+}
+
+# Tests call this to initialize the workflow factory with common
+# information
+
 sub init_factory {
     my $factory = Workflow::Factory->instance;
     $factory->add_config_from_file(
@@ -45,7 +79,8 @@ sub init_factory {
     );
 }
 
-# Initialize the logger and other resources
+# Initialize the logger and other resources; called when module
+# required by test
 
 sub init {
     if ( -f $LOG_FILE ) {
