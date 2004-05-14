8d51ce522f25062d6890c8d46f7af9090013e0fe
Chris Winters <chris.winters@gmail.com>
2004-05-14 05:14:22 +0000

    no longer at this level

--- eg/parse.pl
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/perl
-
-use strict;
-use lib qw( ../lib );
-use Data::Dumper qw( Dumper );
-use Workflow::Config;
-
-my ( $type, $file ) = @ARGV;
-print Dumper( Workflow::Config->parse( $type, $file ) );
