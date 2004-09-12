3c1aaed7afd67457a0aeab753c8c8a2f14486970
Chris Winters <chris.winters@gmail.com>
2004-09-12 17:01:24 +0000

    update the history whenever the ticket is updated

--- eg/ticket/App/Action/TicketUpdate.pm
+++ eg/ticket/App/Action/TicketUpdate.pm
@@ -5,6 +5,7 @@ package App::Action::TicketUpdate;
 use strict;
 use base qw( Workflow::Action );
 use Log::Log4perl qw( get_logger );
+use Workflow::History;
 
 $App::Action::TicketUpdate::VERSION  = sprintf("%d.%02d", q$Revision$ =~ /(\d+)\.(\d+)/);
 
@@ -15,6 +16,17 @@ sub execute {
     my $ticket = $wf->context->param( 'ticket' );
     $ticket->status( $wf->state );
     $ticket->update;
+
+    my $current_user = $wf->context->param( 'current_user' );
+    $wf->add_history(
+        Workflow::History->new({
+            action      => 'Ticket update',
+            description => sprintf( 'Ticket updated by %s', $current_user ),
+            user        => $current_user,
+            state       => $wf->state,
+        })
+    );
+    $log->info( "History record added to workflow ok" );
 }
 
 1;
