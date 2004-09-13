ff9fdcf959557108523a7d4a9df3258278723609
Chris Winters <chris.winters@gmail.com>
2004-09-13 02:03:09 +0000

    add implementations for configuration parsers: one for XML, one for Perl

---
+++ workflow/lib/Workflow/Config/Perl.pm
@@ -0,0 +1,116 @@
+package Workflow::Config::Perl;
+
+# $Id$
+
+use strict;
+use base qw( Workflow::Config );
+
+$Workflow::Config::Perl::VERSION  = sprintf("%d.%02d", q$Revision$ =~ /(\d+)\.(\d+)/);
+
+my ( $log );
+
+sub parse {
+    my ( $self, $type, @items ) = @_;
+    $log ||= get_logger();
+
+    $self->_check_config_type( $type );
+    my @config_items = _expand_refs( @items );
+    return () unless ( scalar @config_items );
+
+    my @config = ();
+    foreach my $item ( @config_items ) {
+        my ( $file_name );
+        if ( ref $item ) {
+            $method = '_translate_perl';
+            $file_name = '[scalar ref]';
+        }
+
+        # $item is a filename...
+        else {
+            $method = '_translate_perl_file';
+            $file_name = $item;
+        }
+        $log->is_info &&
+            $log->info( "Will parse '$type' Perl config file '$file_name'" );
+        my $this_config = $class->$method( $type, $item );
+        $log->is_info &&
+            $log->info( "Parsed Perl '$file_name' ok" );
+        if ( ref $this_config->{ $type } eq 'ARRAY' ) {
+            $log->debug( "Adding multiple configurations for '$type'" );
+            push @config, @{ $this_config->{ $type } };
+        }
+        else {
+            $log->debug( "Adding single configuration for '$type'" );
+            push @config, $this_config;
+        }
+    }
+    return @config;
+}
+
+sub _translate_perl_file {
+    my ( $class, $type, $file ) = @_;
+    my $log = get_logger();
+
+    local $/ = undef;
+    open( CONF, '<', $file )
+        || configuration_error "Cannot read file '$file': $!";
+    my $config = <CONF>;
+    close( CONF );
+    my $data = $class->_translate_perl( $type, $config, $file );
+    $log->is_debug &&
+        $log->debug( "Translated '$type' '$file' into: ", Dumper( $data ) );
+    return $data;
+}
+
+sub _translate_perl {
+    my ( $class, $type, $config, $file ) = @_;
+    my $log = get_logger();
+
+    no strict 'vars';
+    my $data = eval $config;
+    if ( $@ ) {
+        configuration_error "Cannot evaluate perl data structure ",
+                            "in '$file': $@";
+    }
+    return $data;
+}
+
+
+
+1;
+
+__END__
+
+=head1 NAME
+
+Workflow::Config::Perl - Parse workflow configurations as Perl data structures
+
+=head1 SYNOPSIS
+
+ # either of these is acceptable
+ my $parser = Workflow::Config->new( 'perl' );
+ my $parser = Workflow::Config->new( 'pl' );
+ 
+ my $conf = $parser->parse( 'condition',
+                            'my_conditions.pl', 'your_conditions.perl' );
+
+=head1 DESCRIPTION
+
+Implementation of configuration parser for serialized Perl data
+structures from files/data. See L<Workflow::Config> for C<parse()>
+description.
+
+=head1 SEE ALSO
+
+L<Workflow::Config>
+
+=head1 COPYRIGHT
+
+Copyright (c) 2004 Chris Winters. All rights reserved.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHORS
+
+Chris Winters E<lt>chris@cwinters.comE<gt>
---
+++ workflow/lib/Workflow/Config/XML.pm
@@ -0,0 +1,124 @@
+package Workflow::Config::XML;
+
+# $Id$
+
+use strict;
+use base qw( Workflow::Config );
+use Log::Log4perl       qw( get_logger );
+use Workflow::Exception qw( configuration_error );
+
+$Workflow::Config::XML::VERSION  = sprintf("%d.%02d", q$Revision$ =~ /(\d+)\.(\d+)/);
+
+my ( $log );
+
+my %XML_OPTIONS = (
+    action => {
+        ForceArray => [ 'action', 'field', 'source_list', 'param', 'validator', 'arg' ],
+        KeyAttr    => [],
+    },
+    condition => {
+        ForceArray => [ 'condition', 'param' ],
+        KeyAttr    => [],
+    },
+    persister => {
+        ForceArray => [ 'persister' ],
+        KeyAttr    => [],
+    },
+    validator => {
+        ForceArray => [ 'validator', 'param' ],
+        KeyAttr    => [],
+    },
+    workflow => {
+        ForceArray => [ 'extra_data', 'state', 'action', 'condition' ],
+        KeyAttr    => [],
+    },
+);
+
+my $XML_REQUIRED = 0;
+
+sub parse {
+    my ( $self, $type, @items ) = @_;
+    $log ||= get_logger();
+
+    $self->_check_config_type( $type );
+    my @config_items = Workflow::Config::_expand_refs( @items );
+    return () unless ( scalar @config_items );
+
+    my @config = ();
+    foreach my $item ( @config_items ) {
+        my $file_name = ( ref $item ) ? '[scalar ref]' : $item;
+        $log->is_info &&
+            $log->info( "Will parse '$type' XML config file '$file_name'" );
+        my $this_config = $self->_translate_xml( $type, $item );
+        $log->is_info &&
+            $log->info( "Parsed XML '$file_name' ok" );
+        if ( ref $this_config->{ $type } eq 'ARRAY' ) {
+            $log->is_debug &&
+                $log->debug( "Adding multiple configurations for '$type'" );
+            push @config, @{ $this_config->{ $type } };
+        }
+        else {
+            $log->is_debug &&
+                $log->debug( "Adding single configuration for '$type'" );
+            push @config, $this_config;
+        }
+    }
+    return @config;
+}
+
+# $config can either be a filename or scalar ref with file contents
+
+sub _translate_xml {
+    my( $self, $type, $config ) = @_;
+    unless ( $XML_REQUIRED ) {
+        eval { require XML::Simple };
+        if ( $@ ) {
+            configuration_error "XML::Simple must be installed to parse ",
+                                "configuration files/data in XML format";
+        }
+        else {
+            XML::Simple->import( ':strict' );
+            $XML_REQUIRED++;
+        }
+    };
+    my $options = $XML_OPTIONS{ $type } || {};
+    my $data = XMLin( $config, %{ $options } );
+    return $data;
+}
+
+1;
+
+__END__
+
+=head1 NAME
+
+Workflow::Config::XML - Parse workflow configurations from XML content
+
+=head1 SYNOPSIS
+
+ my $parser = Workflow::Config->new( 'xml' );
+ my $conf = $parser->parse( 'condition',
+                            'my_conditions.xml', 'your_conditions.xml' );
+
+=head1 DESCRIPTION
+
+Implementation of configuration parser for XML files/data; requires
+L<XML::Simple> to be installed. See L<Workflow::Config> for C<parse()>
+description.
+
+=head1 SEE ALSO
+
+L<XML::Simple>
+
+L<Workflow::Config>
+
+=head1 COPYRIGHT
+
+Copyright (c) 2004 Chris Winters. All rights reserved.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHORS
+
+Chris Winters E<lt>chris@cwinters.comE<gt>
