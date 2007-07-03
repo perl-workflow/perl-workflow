b37c8be4b1b07daa88719165436199be4db6116a
Jonas B. Nielsen <jonasbn@logicLAB.dk>
2007-07-03 15:13:28 +0000

    - Added missing version number to Workflow::Persister (1.09), the PAUSE indexer   failed for this module, I am not sure what this means, but I will find out

--- lib/Workflow/Persister.pm
+++ lib/Workflow/Persister.pm
@@ -7,7 +7,7 @@ use base qw( Workflow::Base );
 use Log::Log4perl       qw( get_logger );
 use Workflow::Exception qw( persist_error );
 
-$Workflow::Persister::VERSION = '';
+$Workflow::Persister::VERSION = '1.09';
 
 my @FIELDS = qw( name class
                  use_random use_uuid
