232228524d965eb75d11a0a07c4580be9cddad35
Jonas B. Nielsen <jonasbn@logicLAB.dk>
2012-11-05 08:05:36 +0000

    Closing release 1.34_4

--- workflow/lib/Workflow.pm
+++ workflow/lib/Workflow.pm
@@ -16,7 +16,7 @@ my @FIELDS = qw( id type description state last_update time_zone );
 my @INTERNAL = qw( _factory );
 __PACKAGE__->mk_accessors( @FIELDS, @INTERNAL );
 
-$Workflow::VERSION = '1.34_3';
+$Workflow::VERSION = '1.34_4';
 
 
 use constant NO_CHANGE_VALUE => 'NOCHANGE';
