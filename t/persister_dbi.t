perl-workflow/perl-workflow

commit e2707cce7561abd7d4370ea86382628bbede364a

Test persister overridden fields (#85)
* Test inherited DBI persister overriding table field names

The DBI persister documents that field names can be overridden. This
test assures that two persisters using different field names can
co-exist.

* Fix: DBI persisters with different field name sets can't co-exist

lib/Workflow/Persister/DBI.pm
@@ -17,13 +17,11 @@ use English qw( -no_match_vars );
 
 $Workflow::Persister::DBI::VERSION = '1.50';
 
-my @FIELDS = qw( handle dsn user password driver
+my @FIELDS = qw( _wf_fields _hist_fields handle dsn user password driver
     workflow_table history_table date_format parser autocommit);
 __PACKAGE__->mk_accessors(@FIELDS);
 
 my ($log);
-my @WF_FIELDS   = ();
-my @HIST_FIELDS = ();
 
 sub init {
     my ( $self, $params ) = @_;
@@ -196,8 +194,9 @@ sub create_workflow {
     my ( $self, $wf ) = @_;
     $log ||= get_logger();
     $self->_init_fields();
-    my @fields = @WF_FIELDS[ 1, 2, 3 ];
-    my @values = (
+    my @wf_fields = @{ $self->_wf_fields };
+    my @fields    = @wf_fields[ 1, 2, 3 ];
+    my @values    = (
         $wf->type,
         $wf->state,
         DateTime->now( time_zone => $wf->time_zone() )
@@ -207,7 +206,7 @@ sub create_workflow {
 
     my $id = $self->workflow_id_generator->pre_fetch_id($dbh);
     if ($id) {
-        push @fields, $WF_FIELDS[0];
+        push @fields, $wf_fields[0];
         push @values, $id;
         $log->is_debug
             && $log->debug("Got ID from pre_fetch_id: $id");
@@ -249,13 +248,16 @@ sub fetch_workflow {
     my ( $self, $wf_id ) = @_;
     $self->_init_fields();
     $log ||= get_logger();
-    my $sql = qq{
-        SELECT $WF_FIELDS[2], $WF_FIELDS[3]
+    my $sql = q{
+        SELECT %s, %s
           FROM %s
-         WHERE $WF_FIELDS[0] = ?
+         WHERE %s = ?
     };
+    my @wf_fields = @{ $self->_wf_fields };
     $sql = sprintf $sql,
-        $self->handle->quote_identifier( $self->workflow_table );
+        $wf_fields[2], $wf_fields[3],
+        $self->handle->quote_identifier( $self->workflow_table ),
+        $wf_fields[0];
 
     if ( $log->is_debug ) {
         $log->debug("Will use SQL\n$sql");
@@ -283,13 +285,15 @@ sub update_workflow {
     my ( $self, $wf ) = @_;
     $self->_init_fields();
     $log ||= get_logger();
-    my $sql = qq{
+    my $sql = q{
         UPDATE %s
-           SET $WF_FIELDS[2] = ?,
-               $WF_FIELDS[3] = ?
-         WHERE $WF_FIELDS[0] = ?
+           SET %s = ?,
+               %s = ?
+         WHERE %s = ?
     };
-    $sql = sprintf $sql, $self->workflow_table;
+    my @wf_fields = @{ $self->_wf_fields };
+    $sql          = sprintf $sql, $self->workflow_table,
+        $wf_fields[2], $wf_fields[3], $wf_fields[0];
     my $update_date = DateTime->now( time_zone => $wf->time_zone() )
         ->strftime( $self->date_format() );
 
@@ -319,13 +323,14 @@ sub create_history {
     foreach my $h (@history) {
         next if ( $h->is_saved );
         my $id     = $generator->pre_fetch_id($dbh);
-        my @fields = @HIST_FIELDS[ 1 .. 6 ];
-        my @values = (
+        my @hist_fields = @{ $self->_hist_fields };
+        my @fields      = @hist_fields[ 1 .. 6 ];
+        my @values      = (
             $wf->id, $h->action, $h->description, $h->state, $h->user,
             $h->date->strftime( $self->date_format() ),
         );
         if ($id) {
-            push @fields, $HIST_FIELDS[0];
+            push @fields, $hist_fields[0];
             push @values, $id;
         }
         my $sql = 'INSERT INTO %s ( %s ) VALUES ( %s )';
@@ -367,11 +372,14 @@ sub fetch_history {
     my $sql = qq{
         SELECT %s
           FROM %s
-         WHERE $HIST_FIELDS[1] = ?
-      ORDER BY $HIST_FIELDS[6] DESC
+         WHERE %s = ?
+      ORDER BY %s DESC
     };
-    my $history_fields = join ', ', @HIST_FIELDS;
-    $sql = sprintf $sql, $history_fields, $self->history_table;
+    my @hist_fields    = @{ $self->_hist_fields };
+    my $history_fields = join ', ', @hist_fields;
+    $sql = sprintf $sql, $history_fields,
+        $hist_fields[1], $hist_fields[6],
+        $self->history_table;
 
     if ( $log->is_debug ) {
         $log->debug("Will use SQL\n$sql");
@@ -440,15 +448,21 @@ sub rollback_transaction {
 
 sub _init_fields {
     my ($self) = @_;
-    unless ( scalar @WF_FIELDS ) {
-        @WF_FIELDS = map {
-            $self->handle->quote_identifier($_)
-        } $self->get_workflow_fields();
+    unless ( $self->_wf_fields ) {
+        $self->_wf_fields(
+            [
+             map {
+                 $self->handle->quote_identifier($_)
+             } $self->get_workflow_fields()
+            ]);
     }
-    unless ( scalar @HIST_FIELDS ) {
-        @HIST_FIELDS = map {
-            $self->handle->quote_identifier($_)
-        } $self->get_history_fields();
+    unless ( $self->_hist_fields ) {
+        $self->_hist_fields(
+            [
+             map {
+                 $self->handle->quote_identifier($_)
+             } $self->get_history_fields()
+            ]);
     }
 }
 

t/PersisterDBIOtherFields.pm
@@ -1,0 +1,17 @@
+package PersisterDBIOtherFields;
+
+use warnings;
+use strict;
+use base qw( Workflow::Persister::DBI );
+
+
+sub get_workflow_fields {
+    return qw( w1 w2 w3 w4 );
+}
+
+sub get_history_fields {
+    return qw( h1 h2 h3 h4 h5 h6 h7 );
+}
+
+1;
+

t/persister_dbi_inherit.t
@@ -1,0 +1,82 @@
+#!/usr/bin/env perl
+
+use strict;
+use lib qw(../lib lib ../t t);
+use Test::More;
+
+eval "require DBI";
+if ( $@ ) {
+    plan skip_all => 'DBI not installed';
+}
+plan tests => 7;
+
+
+my $TICKET_CLASS = 'TestApp::Ticket';
+my $DATE_FORMAT = '%Y-%m-%d %H:%M';
+
+require_ok( 'PersisterDBIOtherFields' );
+require_ok( 'Workflow::Persister::DBI' );
+require_ok( 'Workflow::Factory' );
+
+
+my @persisters = (
+    {
+        class  => 'Workflow::Persister::DBI',
+        name   => 'Regular',
+        dsn    => 'dbi:Mock:'
+    },
+    {
+        class  => 'PersisterDBIOtherFields',
+        name   => 'Other',
+        dsn    => 'dbi:Mock:'
+    }
+    );
+
+my $wf_config_main = {
+    type        => 'main',
+    description => 'main',
+    persister   => 'Regular',
+    state       => [
+        {
+            name => 'INITIAL',
+        },
+        ],
+};
+my $wf_config_other = {
+    type        => 'other',
+    description => 'other',
+    persister   => 'Other',
+    state       => [
+        {
+            name => 'INITIAL',
+        },
+        ],
+};
+
+my $factory = Workflow::Factory->instance;
+$factory->add_config( persister => \@persisters );
+$factory->add_config( workflow  => $wf_config_main );
+$factory->add_config( workflow  => $wf_config_other );
+
+my $wf_main  = $factory->create_workflow( 'main' );
+my $wf_other = $factory->create_workflow( 'other' );
+
+
+my $main_history  = $factory->get_persister( 'Regular' )->handle->{mock_all_history};
+my $other_history = $factory->get_persister( 'Other' )->handle->{mock_all_history};
+
+like( $main_history->[0]->{statement},
+      qr/\QINSERT INTO "workflow" ( "type", "state", "last_update", "workflow_id" ) \E/,
+      'Regular workflow field names used with standard DBI persister');
+like( $other_history->[0]->{statement},
+      qr/\QINSERT INTO "workflow" ( "w2", "w3", "w4", "w1" ) \E/,
+      'Overridden workflow field names used with DBI-persister derived class');
+
+like( $main_history->[1]->{statement},
+      qr/\QINSERT INTO "workflow_history" ( "workflow_id", "action", "description", "state", "workflow_user", "history_date", "workflow_hist_id" ) \E/,
+      'Regular history field names used with standard DBI persister');
+like( $other_history->[1]->{statement},
+      qr/\QINSERT INTO "workflow_history" ( "h2", "h3", "h4", "h5", "h6", "h7", "h1" ) \E/,
+      'Overridden history field names used with DBI-persister derived class');
+
+

commit 004f94a0620d9c6408827917e3e7913bcecbb3df

Fix #95, incorrect SQL interpolation caused by #85

lib/Workflow/Persister/DBI.pm
@@ -378,8 +378,8 @@ sub fetch_history {
     my @hist_fields    = @{ $self->_hist_fields };
     my $history_fields = join ', ', @hist_fields;
     $sql = sprintf $sql, $history_fields,
-        $hist_fields[1], $hist_fields[6],
-        $self->history_table;
+        $self->handle->quote_identifier($self->history_table),
+        $hist_fields[1], $hist_fields[6];
 
     if ( $log->is_debug ) {
         $log->debug("Will use SQL\n$sql");

t/persister_dbi.t
@@ -3,7 +3,7 @@
 use strict;
 use lib qw(../lib lib ../t t);
 use TestUtil;
-use constant NUM_TESTS => 46;
+use constant NUM_TESTS => 50;
 use Test::More;
 use Test::Exception;
 
@@ -157,4 +157,30 @@ my ( $wf );
     is( $wf, undef,
         'Trying to fetch non-existent workflow returns undef' );
 
+}
+
+
+{
+    $handle->{mock_clear_history} = 1;
+    my $wf = $factory->create_workflow( 'Ticket' );
+    my $wf_hist = $handle->{mock_all_history};
+    $handle->{mock_clear_history} = 1;
+    $handle->{mock_add_resultset} =
+        [
+         [ qw/workflow_hist_id workflow_id
+           action description state workflow_user
+              history_date / ],
+         [ 'def', $wf->id, 'Create workflow', 'Create new workflow',
+          'INITIAL', 'n/a', $wf_hist->[1]->{bound_params}->[5] ]
+        ];
+    my @history = $wf->get_history();
+
+
+    my $stmt = $handle->{mock_all_history}->[0]->statement;
+    like($stmt,
+         qr/SELECT "workflow_hist_id", "workflow_id", "action", "description", "state", "workflow_user", "history_date"/,
+         'Quote workflow history table identifiers');
+    like($stmt, qr/FROM "workflow_history"/, 'Query from "workflow_history" table');
+    like($stmt, qr/WHERE "workflow_id" = ?/, 'Query history by "workflow_id"');
+    like($stmt, qr/ORDER BY "history_date" DESC/, 'Ordering on "history_date"');
 }